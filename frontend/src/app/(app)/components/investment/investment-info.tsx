308f832ee5d095b0306a2602983b6c89476c12e4
Matej Lörinc <matejlorinc1@gmail.com>
2025-02-21 08:57:04 +0100

    Remove click effect on investment info

--- frontend/src/app/(app)/components/investment/investment-info.tsx
+++ frontend/src/app/(app)/components/investment/investment-info.tsx
@@ -2,7 +2,7 @@ import {ReactElement} from "react";
 
 export function InvestmentInfoComponent({header, info, icon}: { header: string, info: string, icon: ReactElement }) {
     return <div
-        className="flex basis-0 flex-grow min-w-64 rounded p-2 px-4 bg-black/5 justify-between items-center cursor-pointer hover:bg-black/10 transition duration-300">
+        className="flex basis-0 flex-grow min-w-64 rounded p-2 px-4 bg-black/5 justify-between items-center">
         <div>
             <p className="font-medium">{header}</p>
             <p className="text-sm">{info}</p>
